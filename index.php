5a30964ed30b27e39ad4a6f583db374d098da3b7
Riska997 <95927125+Riska997@users.noreply.github.com>
2024-11-04 18:00:49 +0200

    update

--- index.php
+++ index.php
@@ -57,7 +57,7 @@
                         <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link js-scroll-trigger" href="customer/login.php">Login</a>
+                        <a class="nav-link js-scroll-trigger" href="login.php">Login</a>
                     </li>
                 </ul>
             </div>
